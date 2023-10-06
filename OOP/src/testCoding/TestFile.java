c5473a1f89bda2d4e2f9ab654c4d63753878eaa6
OleSun <127397159+OleSun@users.noreply.github.com>
2023-10-06 10:37:09 +0200

    Create TestFile.java

---
+++ OOP/src/testCoding/TestFile.java
@@ -0,0 +1,14 @@
+package testCoding;
+
+public class TestFile {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String test1 = "Hope this works";
+		
+		System.out.println(test1);
+	}
+
+}
